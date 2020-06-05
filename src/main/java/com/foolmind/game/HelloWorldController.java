5f6feb7f109bd6486a9ab6c62472c43c03ae8461
shivratangupta <shivratangupta18@gmail.com>
2020-06-05 22:38:35 +0530

    add Basic setup

---
+++ src/main/java/com/foolmind/game/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.foolmind.game;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dev-test")
+public class HelloWorldController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
