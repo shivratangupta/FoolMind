10269ee73b6b62f72a38551b5f82297adc9b5055
shivratangupta <shivratangupta18@gmail.com>
2020-06-06 04:16:05 +0530

    add Player class and Auditable class

--- src/main/java/com/foolmind/game/model/Auditable.java
+++ src/main/java/com/foolmind/game/model/Auditable.java
@@ -1,4 +1,36 @@
 package com.foolmind.game.model;
 
-public class Auditable {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable implements Serializable {
+    @Id
+    @GeneratedValue(generator = "sequence",
+            strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "sequence", allocationSize = 10)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @Getter
+    @Setter
+    private Date createAt = new Date();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private Date updatedAt = new Date();
 }
--- src/main/java/com/foolmind/game/model/Player.java
+++ src/main/java/com/foolmind/game/model/Player.java
@@ -1,4 +1,22 @@
 package com.foolmind.game.model;
 
-public class Player {
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "players")
+public class Player extends Auditable {
+    @NotBlank
+    private String alias;
+
+    @Email @NotBlank
+    private String email;
+
+    @NotBlank
+    private String saltedHashedPassword;
+
+    private String psychFaceURL;
+    private String picURL;
 }
