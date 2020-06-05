17d406da0e57fb5956135d8678705464cb23c052
shivratangupta <shivratangupta18@gmail.com>
2020-06-05 23:55:23 +0530

    set java runtime to java 8

--- src/main/java/com/foolmind/game/HelloWorldController.java
+++ src/main/java/com/foolmind/game/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/dev-test")
 public class HelloWorldController {
+
     @GetMapping("/")
     public String hello() {
         return "Hello, World!";
