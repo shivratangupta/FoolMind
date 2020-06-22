2440705e8c1fb0847a6c053ba17f8b4066fb39fb
shivratangupta <shivratangupta18@gmail.com>
2020-06-23 01:51:59 +0530

    add more data in populateDB endpoint

--- src/main/java/com/foolmind/game/services/DevTestService.java
+++ src/main/java/com/foolmind/game/services/DevTestService.java
@@ -55,22 +55,45 @@ public class DevTestService {
 
         Player reyaan = new Player.Builder()
                 .alias("reyaan")
-                .email("reyaan@gmail.com")
-                .saltedHashedPassword("password")
+                .picURL("https://www.anime-planet.com/images/characters/tanjirou-kamado-145804.jpg")
+                .foolFaceURL("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYxu5_eX0q1LCdfuioGV5lZ4xUz0hAgUI4BiIOx2-Z0Lra8k7G")
+                .email("reyaan@foolmind.com")
+                .saltedHashedPassword("reyaan")
                 .build();
         playerRepository.save(reyaan);
         Player shriyan = new Player.Builder()
                 .alias("shriyan")
-                .email("shriyan@gmail.com")
-                .saltedHashedPassword("password")
+                .picURL("https://i.pinimg.com/originals/85/dd/17/85dd1702345997e97d3660ee677cb551.png")
+                .foolFaceURL("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYxu5_eX0q1LCdfuioGV5lZ4xUz0hAgUI4BiIOx2-Z0Lra8k7G")
+                .email("shriyan@foolmind.com")
+                .saltedHashedPassword("shriyan")
                 .build();
         playerRepository.save(shriyan);
+        Player areana = new Player.Builder()
+                .alias("areana")
+                .picURL("https://images.discordapp.net/avatars/568083171455795200/17e3e43a6072802a0f351e5c3e5d9fc2.png?size=512")
+                .foolFaceURL("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYxu5_eX0q1LCdfuioGV5lZ4xUz0hAgUI4BiIOx2-Z0Lra8k7G")
+                .email("areana@foolmind.com")
+                .saltedHashedPassword("areana")
+                .build();
+        playerRepository.save(shriyan);
+        Player ajayesh = new Player.Builder()
+                .alias("ajayesh")
+                .picURL("https://www.anime-planet.com/images/characters/zenitsu-agatsuma-145870.jpg")
+                .foolFaceURL("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSYxu5_eX0q1LCdfuioGV5lZ4xUz0hAgUI4BiIOx2-Z0Lra8k7G")
+                .email("ajayesh@foolmind.com")
+                .saltedHashedPassword("ajayesh")
+                .build();
+        playerRepository.save(ajayesh);
 
-        GameMode isThisAFact = new GameMode("Is This A Fact?", "/images/is_this_a_fact.jpg", "is this a fact description");
+        GameMode isThisAFact = new GameMode("Is This A Fact?", "/images/is_this_a_fact.jpg", "These remarkable facts may surprise you, but so will your friends' fake answers! Come up with your own answers, and win a point for every player you fool into thinking your false fact is right. Then try to pick the real fact!");
         gameModeRepository.save(isThisAFact);
-        gameModeRepository.save(new GameMode("Word Up", "/images/word_up.png", "word up description"));
+        gameModeRepository.save(new GameMode("Proverbs", "/images/proverb.jpeg", "A half truth is a whole lie. Take these proverbs and write your own endings. Win a point for every player you fool into thinking your ending is right, then try to pick the real one!"));
+        gameModeRepository.save(new GameMode("Animals", "/images/animal.png", "You won't believe these animal facts, because most of them will be made up by you and your friends! Write a fake answer to these real animal facts."));
+        gameModeRepository.save(new GameMode("And Truth Comes Out", "/images/truth.jpeg", "\"If David were arrested tomorrow, it would probably be for this.\" In this mode, you and your friends become the game! Write the best answer about your friends, then choose your favorite answers."));
+        gameModeRepository.save(new GameMode("Word Up", "/images/word_up.png", "This mode is packed with real definitions to unusual words. Make up a word for each definition and win a point for every player you fool into thinking that your word is correct. Then try to pick the real one!"));
         gameModeRepository.save(new GameMode("Un-Scramble", "/images/unscramble.png", "unscramble description"));
-        gameModeRepository.save(new GameMode("Movie Buff", "/images/movie_buff.jpeg", "movie buff description"));
+        gameModeRepository.save(new GameMode("Movie Buff", "/images/movie_buff.jpeg", "These movie titles all belong to very real movies. Make up your own plot and win a point for every player you fool into thinking your plot is the correct one. Then try to pick the real movie plot!"));
 
         List<Question> questions = new ArrayList<>();
         for(Map.Entry<String, String> fileMode : Constants.qaFilesMap.entrySet()) {
@@ -81,13 +104,6 @@ public class DevTestService {
             }
         }
         questionRepository.saveAll(questions);
-
-        Game game = new Game(isThisAFact, 5, true, reyaan);
-        game.addPlayer(shriyan);
-        gameRepository.save(game);
-
-        game.startGame(reyaan);
-        gameRepository.save(game);
 
         return "populated";
     }
